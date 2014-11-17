902b7218d657c6433fb085f25e692d57ef16d307
OA Wu <comdan66@gmail.com>
2014-11-17 22:36:31 +0800

    Fix bug.

--- view/resource/oa_routes/oa_routes.js
+++ view/resource/oa_routes/oa_routes.js
@@ -43,14 +43,15 @@
   function OaRoutes (opt) {
     var extend=function(){var e,t,n,r,i,s,o=arguments[0]||{},u=1,a=arguments.length,f=false;if(typeof o==="boolean"){f=o;o=arguments[1]||{};u=2}if(typeof o!=="object"&&!jQuery.isFunction(o)){o={}}if(a===u){o=this;--u}for(;u<a;u++){if((i=arguments[u])!=null){for(r in i){e=o[r];n=i[r];if(o===n){continue}if(f&&n&&(jQuery.isPlainObject(n)||(t=jQuery.isArray(n)))){if(t){t=false;s=e&&jQuery.isArray(e)?e:[]}else{s=e&&jQuery.isPlainObject(e)?e:{}}o[r]=jQuery.extend(f,s,n)}else if(n!==undefined){o[r]=n}}}}return o}
 
-    this.setPaths = function (opt) {extend (option.path, opt);};
-    this.setFrame = function (opt) {extend (option.frame, opt);};
-    this.setResourceCssList = function (opt) {extend (option.css.resource, opt);};
-    this.setPublicCssList = function (opt) {extend (option.css.public, opt);};
-    this.setResourceJsList = function (opt) {extend (option.js.resource, opt);};
-    this.setPublicJsList = function (opt) {extend (option.js.public, opt);};
-    this.setRouteConfig = function (opt) {extend (option.routes.config, opt);};
-    this.setRoutePageList = function (opt) {extend (option.routes.pages, opt);};
+    this.init = function () {this.loadCss ().loadJs ().loadPage ();};
+    this.setPaths = function (opt) {extend (option.path, opt);return this;};
+    this.setFrame = function (opt) {extend (option.frame, opt);return this;};
+    this.setResourceCssList = function (opt) {extend (option.css.resource, opt);return this;};
+    this.setPublicCssList = function (opt) {extend (option.css.public, opt);return this;};
+    this.setResourceJsList = function (opt) {extend (option.js.resource, opt);return this;};
+    this.setPublicJsList = function (opt) {extend (option.js.public, opt);return this;};
+    this.setRouteConfig = function (opt) {extend (option.routes.config, opt);return this;};
+    this.setRoutePageList = function (opt) {extend (option.routes.pages, opt);return this;};
 
     this.loadCss = function () {
       var head = document.getElementsByTagName('head')[0];
@@ -66,6 +67,7 @@
         domLink.setAttribute ('type', 'text/css');
         head.insertBefore (domLink,script);
       });
+      return this;
     };
 
     this.loadJs = function () {
@@ -82,6 +84,7 @@
         domLink.setAttribute ('type', 'text/javascript');
         head.appendChild (domLink);
       });
+      return this;
     };
 
     this.loadPage = function () {
@@ -107,12 +110,12 @@
           $('<script />').attr ('data-content', content).attr ('src', option.path.content + content + '/content.js').attr ('language', 'javascript').attr ('type', 'text/javascript').appendTo ('head');
 
           ajax (option.path.content + content + '/content.html', function (r) {
-            var titles = r.match (/<title.*?>([\s\S]*)<\/title>/gi).map (function (t) { return /<title.*?>([\s\S]*)<\/title>/.exec (t)[1].trim (); }).filter (function (t) { return t; });
-            if (titles.length) $('html head title').html (titles[0].trim ());
-            var $title = $('html head title');
-            r.match (/<meta.*\/?>/gi).reverse ().map (function (t) {$(t).attr ('data-content', content).insertAfter ($title);});
-            var bodies = r.match (/<body.*?>([\s\S]*)<\/body>/gi).map (function (t) { return /<body.*?>([\s\S]*)<\/body>/.exec (t)[1].trim (); }).filter (function (t) { return t; });
-            if (bodies.length) $(option.frame.selector).html (bodies[0]);
+            var titles = r.match (/<title.*?>([\s\S]*)<\/title>/gi);
+            if (titles && titles.length && (titles = titles.map (function (t) { return /<title.*?>([\s\S]*)<\/title>/.exec (t)[1].trim (); }).filter (function (t) { return t; })) && titles.length && (titles = titles[0])) $('html head title').html (titles);
+            var metas = r.match (/<meta.*\/?>/gi);
+            if (metas && metas.length) metas.reverse ().map (function (t) {$(t).attr ('data-content', content).insertAfter ('html head title');});
+            var bodies = r.match (/<body.*?>([\s\S]*)<\/body>/gi);
+            if (bodies && bodies.length && (bodies = bodies.map (function (t) { return /<body.*?>([\s\S]*)<\/body>/.exec (t)[1].trim (); }).filter (function (t) { return t; })) && bodies.length && (bodies = bodies[0])) $(option.frame.selector).html (bodies);
           });
         };
         var setRoutes = function () {
@@ -124,13 +127,14 @@
         }
 
         ajax (option.path.frame + option.frame.name + '/frame.html', function (r) {
-          var titles = r.match (/<title.*?>([\s\S]*)<\/title>/gi).map (function (t) { return /<title.*?>([\s\S]*)<\/title>/.exec (t)[1].trim (); }).filter (function (t) { return t; });
-          if (titles.length) $('html head title').html (titles[0].trim ());
-          var bodies = r.match (/<body.*?>([\s\S]*)<\/body>/gi).map (function (t) { return /<body.*?>([\s\S]*)<\/body>/.exec (t)[1].trim (); }).filter (function (t) { return t; });
-          if (bodies.length) $('html body').html (bodies[0]);
+          var titles = r.match (/<title.*?>([\s\S]*)<\/title>/gi);
+          if (titles && titles.length && (titles = titles.map (function (t) { return /<title.*?>([\s\S]*)<\/title>/.exec (t)[1].trim (); }).filter (function (t) { return t; })) && titles.length && (titles = titles[0])) $('html head title').html (titles);
+          var bodies = r.match (/<body.*?>([\s\S]*)<\/body>/gi);
+          if (bodies && bodies.length && (bodies = bodies.map (function (t) { return /<body.*?>([\s\S]*)<\/body>/.exec (t)[1].trim (); }).filter (function (t) { return t; })) && bodies.length && (bodies = bodies[0])) $('html body').html (bodies);
           setRoutes ();
         });
       }
+      return this;
     };
 
     extend (option, opt);
